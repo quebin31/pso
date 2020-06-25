041c46f16536900bff52f20752871fa111a7c287
Kevin Del Castillo <quebin31@gmail.com>
2020-06-25 10:41:25 -0500

    Fixed update of value and update of best position

--- src/pso.rs
+++ src/pso.rs
@@ -1,8 +1,13 @@
 use crate::fitness::Fitness;
+use anyhow::Error;
 use ndarray::Array1;
 use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
+use plotters::coord::Shift;
+use plotters::drawing::backend::DrawingBackend;
+use plotters::prelude::*;
 use rand::{thread_rng, Rng};
+use std::fmt::Error as FmtError;
 
 #[derive(Debug, Clone)]
 pub struct Particle {
@@ -12,15 +17,27 @@ pub struct Particle {
 }
 
 impl Particle {
-    fn new(dim: usize, value_range: &Uniform<f64>, velocity_range: &Uniform<f64>) -> Self {
-        let value = Array1::random((dim,), value_range);
-        let velocity = Array1::random((dim,), velocity_range);
+    fn new(dim: usize, value_distr: &Uniform<f64>, velocity_distr: &Uniform<f64>) -> Self {
+        let value = Array1::random((dim,), value_distr);
+        let velocity = Array1::random((dim,), velocity_distr);
 
         Self {
             curr_value: value.clone(),
             best_value: value,
             velocity,
         }
+    }
+
+    pub fn best(&self) -> &Array1<f64> {
+        &self.best_value
+    }
+
+    pub fn value(&self) -> &Array1<f64> {
+        &self.curr_value
+    }
+
+    pub fn velocity(&self) -> &Array1<f64> {
+        &self.velocity
     }
 
     fn update_velocity(&mut self, global_best: &Array1<f64>, mut rng: impl Rng, options: &Options) {
@@ -36,18 +53,21 @@ impl Particle {
         let rand_2 = rng.gen_range(0.0, 1.0);
         let trd_term = (global_best - &self.curr_value).mapv(|v| v * phi_2 * rand_2);
 
+        println!("rand_1: {}", rand_1);
+        println!("rand_2: {}", rand_2);
+
         self.velocity = fst_term + snd_term + trd_term;
     }
 
     fn update_value(&mut self) {
-        self.curr_value = &self.curr_value - &self.velocity;
+        self.curr_value = &self.curr_value + &self.velocity;
     }
 
     fn update_best(&mut self, fitness: &Fitness<Array1<f64>>) {
         let curr_fitness = fitness.calculate_for_maximization(&self.curr_value);
         let best_fitness = fitness.calculate_for_maximization(&self.best_value);
 
-        if curr_fitness < best_fitness {
+        if best_fitness < curr_fitness {
             self.best_value = self.curr_value.clone();
         }
     }
@@ -81,8 +101,8 @@ impl<'a> Particles<'a> {
         let global_best = particles
             .iter()
             .max_by(|a, b| {
-                let fa = fitness.calculate_for_maximization(&a.curr_value);
-                let fb = fitness.calculate_for_maximization(&b.curr_value);
+                let fa = fitness.calculate_for_maximization(&a.value());
+                let fb = fitness.calculate_for_maximization(&b.value());
 
                 fa.partial_cmp(&fb).expect("Received a NaN")
             })
@@ -107,18 +127,26 @@ impl<'a> Particles<'a> {
             Some(rng.gen_range(0.0, 1.0))
         };
 
-        for particle in &mut self.particles {
+        println!("Omega (ω): {}", options.omega.unwrap());
+
+        for (i, particle) in &mut self.particles.iter_mut().enumerate() {
             particle.update_velocity(&self.global_best, &mut rng, &options);
             particle.update_value();
             particle.update_best(&self.fitness);
+            println!(
+                "{}) x: {}, v: {}",
+                i + 1,
+                particle.value(),
+                particle.velocity()
+            );
         }
 
         let local_best = self
             .particles
             .iter()
             .max_by(|a, b| {
-                let fa = self.fitness.calculate_for_maximization(&a.curr_value);
-                let fb = self.fitness.calculate_for_maximization(&b.curr_value);
+                let fa = self.fitness.calculate_for_maximization(&a.value());
+                let fb = self.fitness.calculate_for_maximization(&b.value());
 
                 fa.partial_cmp(&fb).expect("Received a NaN")
             })
@@ -128,13 +156,100 @@ impl<'a> Particles<'a> {
 
         let best_fitness = self.fitness.calculate_for_maximization(&self.global_best);
         let local_fitness = self.fitness.calculate_for_maximization(&local_best);
+        println!(
+            "Mejor en esta iteración: x: {}, fitness: {}",
+            local_best,
+            self.fitness.calculate(&local_best)
+        );
 
         if best_fitness < local_fitness {
+            println!("El mejor global cambió");
             self.global_best = local_best;
         }
     }
 
     pub fn best(&self) -> &Array1<f64> {
         &self.global_best
+    }
+
+    pub fn particles(&self) -> &Vec<Particle> {
+        &self.particles
+    }
+
+    pub fn summary(&self, show_particles: bool) -> Result<String, FmtError> {
+        use std::fmt::Write;
+
+        let mut particles_out = String::new();
+        let mut fitness_out = String::new();
+        let mut blocals_out = String::new();
+
+        if show_particles {
+            writeln!(particles_out, ">>> Cúmulo de partículas <<<")?;
+        }
+
+        writeln!(fitness_out, ">>> Fitness <<<")?;
+        writeln!(blocals_out, ">>> Mejores locales <<<")?;
+
+        for (i, particle) in self.particles.iter().enumerate() {
+            if show_particles {
+                writeln!(
+                    particles_out,
+                    "{}) x: {},  v: {}",
+                    i + 1,
+                    particle.value(),
+                    particle.velocity()
+                )?;
+            }
+
+            let fitness = self.fitness.calculate(particle.value());
+            writeln!(fitness_out, "{}) {}", i + 1, fitness)?;
+
+            let best_fitness = self.fitness.calculate(particle.best());
+            writeln!(
+                blocals_out,
+                "{}) x: {}, fitness: {}",
+                i + 1,
+                particle.best(),
+                best_fitness
+            )?;
+        }
+
+        let best_fitness = self.fitness.calculate(self.best());
+        let best_global = format!(
+            ">>> Mejor global: x: {}, fitness: {}",
+            self.best(),
+            best_fitness
+        );
+
+        let out = format!(
+            "{}{}{}{}",
+            particles_out, fitness_out, blocals_out, best_global
+        );
+
+        Ok(out)
+    }
+
+    pub fn plot<D>(&self, root: &DrawingArea<D, Shift>, i: usize) -> Result<(), Error>
+    where
+        D: DrawingBackend,
+        <D as DrawingBackend>::ErrorType: 'static,
+    {
+        root.fill(&WHITE)?;
+
+        let mut chart = ChartBuilder::on(root)
+            .set_label_area_size(LabelAreaPosition::Left, 50)
+            .set_label_area_size(LabelAreaPosition::Bottom, 50)
+            .set_label_area_size(LabelAreaPosition::Right, 50)
+            .caption(format!("PSO (iter = {})", i), ("sans-serif", 50))
+            .build_ranged(-5.0..5.0, -5.0..5.0)?;
+
+        chart.configure_mesh().draw()?;
+        chart.draw_series(self.particles.iter().map(|p| {
+            let value = p.value();
+            let center = (value[0], value[1]);
+            Circle::new(center, 5, ShapeStyle::from(&BLUE).filled())
+        }))?;
+
+        Ok(root.present()?)
     }
 }
